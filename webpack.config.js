b1420cea4750102203282615b46c05c1beac5ab7
matiaslgonzalez <matias041198@gmail.com>
2023-04-20 00:32:37 -0400

    Added babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
-module: {
-    loaders: [
-        // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-        { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
-    ],
-    preLoaders: [
-        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-        { test: /\.js$/, loader: "source-map-loader" }
-    ]
-}
-
+export const module = {
+	rules: [
+		// All files with a '.ts' or '.tsx' extension will be handled by 'babel-loader'.
+		{
+			test: /\.tsx?$/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: [
+						'@babel/preset-env',
+						'@babel/preset-typescript',
+					],
+				},
+			},
+			exclude: /node_modules/,
+		},
+		// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+		{ test: /\.js$/, loader: "source-map-loader" },
+	],
+};
+export const resolve = {
+	extensions: ['.tsx', '.ts', '.js'],
+};
