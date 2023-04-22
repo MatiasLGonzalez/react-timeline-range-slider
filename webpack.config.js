d2130a223c6d84f383106ab79a6f73408f3fd6a9
MatiasLGonzalez <matias041198@gmail.com>
2023-04-22 08:29:34 -0400

    Fixed webpack.config.js syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
-export const module = {
-  rules: [
-    // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-    {
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/,
-    },
-    // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-    { test: /\.js$/, loader: 'source-map-loader' },
-  ],
-};
-export const resolve = {
-  extensions: ['.tsx', '.ts', '.js'],
-};
+const config = {
+	module: {
+	  rules: [
+		{
+		  test: /\.tsx?$/,
+		  use: 'ts-loader',
+		  exclude: /node_modules/,
+		},
+		{ test: /\.js$/, loader: 'source-map-loader' },
+	  ],
+	},
+	resolve: {
+	  extensions: ['.tsx', '.ts', '.js'],
+	},
+  };
+  
+  module.exports = config;
+  
