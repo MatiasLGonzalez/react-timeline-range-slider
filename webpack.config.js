d21282774fc9924bcdcfef54b27147d118e1f50c
MatiasLGonzalez <matias041198@gmail.com>
2023-04-22 08:08:15 -0400

    Changing to ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,15 @@
 export const module = {
   rules: [
-    // All files with a '.ts' or '.tsx' extension will be handled by 'babel-loader'.
+    // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
     {
       test: /\.tsx?$/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: [
-            "@babel/preset-env",
-            ["@babel/preset-typescript", { allowDeclareFields: true }],
-          ],
-        },
-      },
+      use: 'ts-loader',
       exclude: /node_modules/,
     },
     // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-    { test: /\.js$/, loader: "source-map-loader" },
+    { test: /\.js$/, loader: 'source-map-loader' },
   ],
 };
 export const resolve = {
-  extensions: [".tsx", ".ts", ".js"],
+  extensions: ['.tsx', '.ts', '.js'],
 };
